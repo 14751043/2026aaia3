7369677946bb94ef2a4c9ea815c85809f0bafe2e
14751043 <14751043@me.mcu.edu.tw>
2026-09-09 11:58:01 +0800

    week01

---
+++ week01/week01-2.cpp
@@ -0,0 +1,16 @@
+// week01-2.cpp
+#include <iostream>
+int main()
+{
+	int N;
+	std::cin >> N;
+	int b = N, ans = 0;
+	while (N>0){
+		ans = ans*10 + N%10;
+		N = N / 10;
+	}
+	std::cout << b << ans << b+ans; // WRONG-ANSWER
+	//std::cout << b << "+" << ans << "=" << b+ans << std::endl;
+	//std::cout << b << "+" << ans << "=" << b+ans << "\n";
+	//printf("%d+%d=%d\n",b ,ans, ans+b)
+}
